ba6b03c0755f864911c6c76301f2e4c658e4f876
Vlad <avgustinovich994@gmail.com>
2020-06-15 00:55:40 +0300

    Update Program.cs

--- src/HomeWork-1/Program.cs
+++ src/HomeWork-1/Program.cs
@@ -7,48 +7,55 @@ namespace HomeWork_1
     {
         static void Main(string[] args)
         {
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            //пользователю нужно сообщить о введении операции
+            Console.Write("Введите номер дня недели(от 1 до 7): ");
+            //считать вывод пользователя и сохранить в переменную
+            var userEnter = Console.ReadLine();
+            //преобразование из одного типа данных в другой
+            int result;
+            var parced = int.TryParse(userEnter, out result);
+            //если пользователь ввёл не число, показать пользователю сообщение об ошибке
+            if (parced == false)
             {
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Научись на цифры нажимать!");
             }
-
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Tuesday)
+            //если пользователь ввёл число, не от одного до семи, тогда вывести сообщение об ошибке
+            else if (result > 7 || result < 1)
             {
-                Console.WriteLine("Today is Tuesday");
+                Console.WriteLine("Тебе же выше написали, от 1 до 7! ");
             }
-
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+            else
             {
-                Console.WriteLine("Today is Wensday!");
+                //если день недели равен одному - тогда будет понедельник, повторить для остальных 6-ти дней
+                if (result == 1)
+                {
+                    Console.WriteLine("Этот день понедельник");
+                }
+                else if (result == 2)
+                {
+                    Console.WriteLine("Этот день вторник");
+                }
+                else if (result == 3)
+                {
+                    Console.WriteLine("Этот день среда");
+                }
+                else if (result == 4)
+                {
+                    Console.WriteLine("Этот день четверг");
+                }
+                else if (result == 5)
+                {
+                    Console.WriteLine("Этот день пятница");
+                }
+                else if (result == 6)
+                {
+                    Console.WriteLine("Этот день суббота");
+                }
+                else if (result == 7)
+                {
+                    Console.WriteLine("Этот день воскресенье");
+                }
             }
-
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
-            {
-                Console.WriteLine("Today is Thursday!");
-            }
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
-            {
-                Console.WriteLine("Today is Friday!");
-            }
-
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
-            {
-                Console.WriteLine("Today is Saturday!");
-            }
-
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
-            {
-                Console.WriteLine("Todat is Sunday!");
-            }
-
-
         }
     }
 }
