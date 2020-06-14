43e1534664bb6d3080354130b18f9dbb8f7d6f96
Vlad <avgustinovich994@gmail.com>
2020-06-14 23:32:57 +0300

    Update Program.cs

--- src/HomeWork-1/Program.cs
+++ src/HomeWork-1/Program.cs
@@ -8,37 +8,46 @@ namespace HomeWork_1
         static void Main(string[] args)
         {
 
-
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)            {
-                Console.WriteLine("Today is Friday!");
-            }
-
             if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
             {
                 Console.WriteLine("Today is Monday!");
             }
+
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                Console.WriteLine("Today is Tuesday");
+            }
+
 
             if (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
             {
                 Console.WriteLine("Today is Wensday!");
             }
 
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                Console.WriteLine("Today is Tuesday");
-            }
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
-            {
-                Console.WriteLine("Today is Saturday!");
-            }
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
-            {
-                Console.WriteLine("Todat is Sunday!");
-            }
+
             if (DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
             {
                 Console.WriteLine("Today is Thursday!");
             }
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                Console.WriteLine("Today is Friday!");
+            }
+
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+            {
+                Console.WriteLine("Today is Saturday!");
+            }
+
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Todat is Sunday!");
+            }
+
 
         }
     }
