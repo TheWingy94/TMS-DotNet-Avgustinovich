37294920156b0653f3cd82cdb1c2137fc449bced
Vlad <avgustinovich994@gmail.com>
2020-06-15 20:33:24 +0300

    fix: replace solutions

---
+++ src/HomeWork -2 (1)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork__2__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
