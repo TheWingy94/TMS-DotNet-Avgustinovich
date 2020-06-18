4ae41f1024d67b380ac77f7f7a2d8b39ec3f93dd
Vlad <avgustinovich994@gmail.com>
2020-06-19 00:24:13 +0300

    feat: add first question

---
+++ src/HomeWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задать вопрос пользователю
+            Console.WriteLine("Какую игру вы хотите? 1-StarWars \t2-Dota2");
+            var a1 = Console.ReadLine();
+            switch (a1)
+            {
+                case "1":
+                    //
+                    break;
+                case "2":
+                    //
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+        }
+    }
+}
